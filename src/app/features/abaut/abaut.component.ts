041423d3c2ce482677786558737689176bd8eff7
Lucas <lucas.hsm15@hotmail.com>
2025-05-26 14:35:50 -0300

    Update version Angular for 19

--- src/app/features/abaut/abaut.component.ts
+++ src/app/features/abaut/abaut.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-abaut',
-  templateUrl: './abaut.component.html',
-  styleUrls: ['./abaut.component.scss']
+    selector: 'app-abaut',
+    templateUrl: './abaut.component.html',
+    styleUrls: ['./abaut.component.scss'],
+    standalone: false
 })
 export class AbautComponent {
 
